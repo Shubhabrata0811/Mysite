e78949cbac5ae1263bb3280baf696c57b3ac45c1
Shubhabrata0811 <shubhabrata.g2017@gmail.com>
2023-04-17 12:21:36 +0530

    add event listeners to elements with the class name nactive and toggle the active class on click.and others

--- jscript/script.js
+++ jscript/script.js
@@ -1,7 +1,77 @@
-/*navigation bar*/
+/*Menu-icon*/
 
-window.onscroll = () =>{
-let header = document.querySelector('.header');
+let menuIcon = document.querySelector('#menuicon');
+let nbar = document.querySelector('.navigationbar');
 
-header.classList.toggle('sticky', window.scrolly > 100);
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    nbar.classList.toggle('active');
 };
+
+
+
+
+/*add event listeners to elements with the class name "nactive" and toggle the "active" class on click. */
+
+var btnCon = document.getElementById("header");
+var btns = Array.from(btnCon.getElementsByClassName("nactive"));
+var sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", function () {
+  // Get the current scroll position
+  var scrollY = window.scrollY;
+
+  // Loop through sections and update the active element
+  sections.forEach(function (sec, index) {
+    var offset = sec.offsetTop - 150;
+    var height = sec.offsetHeight;
+    var id = sec.getAttribute("id");
+
+    if (scrollY >= offset && scrollY < offset + height) {
+      // Remove active class from current active element
+      var curr = document.querySelector(".active");
+      if (curr) {
+        curr.classList.remove("active");
+        curr.classList.add("nactive");
+      }
+
+      // Add active class to corresponding button
+      var btn = btns[index];
+      btn.classList.remove("nactive");
+      btn.classList.add("active");
+    } else {
+      // Remove active class from buttons corresponding to sections not in view
+      var btn = btns[index];
+      btn.classList.remove("active");
+      btn.classList.add("nactive");
+    }
+  });
+});
+
+// Add click event listeners to buttons
+btns.forEach(function (btn, index) {
+  btn.addEventListener("click", function () {
+    var curr = document.querySelector(".active");
+    if (curr) {
+      curr.classList.remove("active");
+      curr.classList.add("nactive");
+    }
+    this.classList.remove("nactive");
+    this.classList.add("active");
+
+    // Scroll to corresponding section
+    sections[index].scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+
+/*Scrolling effect*/
+
+ScrollReveal({
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.main1image', { origin: 'top' });
